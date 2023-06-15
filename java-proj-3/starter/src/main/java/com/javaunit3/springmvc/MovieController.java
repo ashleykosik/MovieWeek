70bd083186fa00d0d1cf3408c1e86ee5976ba809
Ashley Kosik <ashley.kosik@gmail.com>
2023-06-15 17:30:53 -0400

    can add movies to supabase database

--- java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieController.java
+++ java-proj-3/starter/src/main/java/com/javaunit3/springmvc/MovieController.java
@@ -1,13 +1,25 @@
 package com.javaunit3.springmvc;
 
+import com.javaunit3.springmvc.model.MovieEntity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MovieController {
+    @Autowired
+    private BestMovieService bestMovieService;
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @RequestMapping("/")
     public String getIndexPage() {
-
         return "index";
     }
 
@@ -23,10 +35,38 @@ public class MovieController {
     }
 
     @RequestMapping("/VoteForBestMovie")
-    public String voteForBestMovie(HTTPServletRequest request, Model model) {
+    public String voteForBestMovie(HttpServletRequest request, Model model) {
         String movieTitle = request.getParameter("movieTitle");
         model.addAttribute("BestMovieVote", movieTitle);
         return "VoteForBestMovie";
     }
+
+    @RequestMapping("/addMovieForm")
+    public String addMovieForm(){
+        return "addMovie";
+    }
+
+    @RequestMapping("/addMovie")
+    public String addMovie(HttpServletRequest request) {
+        String movieTitle = request.getParameter("movieTitle");
+        String maturityRating = request.getParameter("maturityRating");
+        String genre = request.getParameter("genre");
+
+        MovieEntity movieEntity = new MovieEntity();
+        movieEntity.setTitle(movieTitle);
+        movieEntity.setMaturityRating(maturityRating);
+        movieEntity.setGenre(genre);
+
+        Session session = sessionFactory.getCurrentSession();
+
+        session.beginTransaction();
+        session.save(movieEntity);
+        session.getTransaction().commit();
+
+        return "addMovie";
+
+    }
+
+
 
 }
